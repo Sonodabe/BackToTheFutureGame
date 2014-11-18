144028cb5832ffe5c7a09d0d732bc875a4086ce5
Brandon Sonoda <sonodabe@muohio.edu>
2014-11-18 12:59:44 -0500

    Added a basic board selection scene
    Board selection loads boards to a scene and chooses which knows which
    one is in front of the camera

---
+++ TonyHawk_2K3/Assets/Scripts/BoardSelection/SelectionBoardController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionBoardController : MonoBehaviour {
+    public float oscillationSpeed = 0.1f;
+    public float amplitude = 3.0f;
+    public float rotationSpeed = 0.8f;
+    private float theta;
+    private float lastY;
+    private bool oscillate;
+
+    // Use this for initialization
+    void Start () {
+        theta = 0;
+        lastY = 0;
+        oscillate = false;
+    }
+
+    public void Oscillate (int caller) {
+        Debug.Log (caller);
+        oscillate = true;
+    }
+
+  
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (oscillate) {
+            theta += oscillationSpeed;
+            float newY = amplitude * Mathf.Sin (theta);
+            float deltaY = newY - lastY;
+
+            transform.Translate (deltaY * transform.up);
+
+            lastY = newY;
+            oscillate = false;
+        }
+    }
+}
